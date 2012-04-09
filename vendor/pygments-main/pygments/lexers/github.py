2f20327f900ecf0cab76c0f963a509eddfdf5a33
Vicent Martí <tanoku@gmail.com>
2012-04-09 14:23:37 -0700

    Screwed up registers in the DASM-16 lexer

--- vendor/pygments-main/pygments/lexers/github.py
+++ vendor/pygments-main/pygments/lexers/github.py
@@ -50,7 +50,7 @@ class Dasm16Lexer(RegexLexer):
 
     def guess_identifier(lexer, match):
         ident = match.group(0)
-        klass = Name.Variable if ident.lower() in lexer.REGISTERS else Name.Label
+        klass = Name.Variable if ident.upper() in lexer.REGISTERS else Name.Label
         yield match.start(), klass, ident
 
     tokens = {
