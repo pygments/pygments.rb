c58248b8492010948486a847eb8abbc13fa2199d
Patrick Toomey <ptoomey3@biasedcoin.com>
2014-04-26 15:21:39 -0500

    add timeout configuration option

--- test/test_pygments.rb
+++ test/test_pygments.rb
@@ -41,6 +41,15 @@ class PygmentsHighlightTest < Test::Unit::TestCase
     assert_equal nil, code
   end
 
+  def test_supports_configurable_timeout
+    code = P.highlight(REDIS_CODE)
+    assert_match 'used_memory_peak_human', code
+    assert_equal 455203, code.bytesize.to_i
+    # Assume highlighting a large file will take more than 1 millisecond
+    code = P.highlight(REDIS_CODE, :timeout => 0.001)
+    assert_equal nil, code
+  end
+
   def test_highlight_works_with_null_bytes
     code = P.highlight("\0hello", :lexer => 'rb')
     assert_match "hello", code
