d7c96047eb4e8ac4111de03bbb19fbd5a0f9b6a5
Franklin Chen <franklinchen@franklinchen.com>
2014-08-08 02:19:12 -0400

    Force timeout_time to be int.

--- lib/pygments/popen.rb
+++ lib/pygments/popen.rb
@@ -215,7 +215,7 @@ module Pygments
 
       begin
         # Timeout requests that take too long.
-        timeout_time = ENV["MENTOS_TIMEOUT"] || 8
+        timeout_time = ENV["MENTOS_TIMEOUT"].to_i || 8
 
         Timeout::timeout(timeout_time) do
           # For sanity checking on both sides of the pipe when highlighting, we prepend and
