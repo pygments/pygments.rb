5aa5990b8d0c197d3d1d3a5d5a8288206f044bf8
Yursen Kim <kyursen@users.noreply.github.com>
2015-12-30 01:04:29 +0300

    Get cache-lexers.rb working on Windows
    Explicitly set binary file mode when writing to file `lexers`.
    It is required to disable line code conversion on Windows.

--- cache-lexers.rb
+++ cache-lexers.rb
@@ -4,5 +4,5 @@ require File.join(File.dirname(__FILE__), '/lib/pygments.rb')
 serialized_lexers = Marshal.dump(Pygments.lexers!)
 
 # Write to a file
-File.open("lexers", 'w') { |file| file.write(serialized_lexers) }
+File.open("lexers", 'wb') { |file| file.write(serialized_lexers) }
 
