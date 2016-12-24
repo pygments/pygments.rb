fb1a6a9929cbf90662adac816762227669e06016
FUJI Goro (gfx) <gfuji@cpan.org>
2016-12-24 12:27:05 +0900

    make it thread-safe

--- lib/pygments.rb
+++ lib/pygments.rb
@@ -1,8 +1,33 @@
 require File.join(File.dirname(__FILE__), 'pygments/popen')
-
+require 'forwardable'
 
 module Pygments
-  extend Pygments::Popen
 
   autoload :Lexer, 'pygments/lexer'
+
+  class << self
+    extend Forwardable
+
+    def engine
+      Thread.current.thread_variable_get(:pygments_engine) ||
+        Thread.current.thread_variable_set(:pygments_engine, Pygments::Popen.new)
+    end
+
+    def_delegators :engine,
+      # public
+      :formatters,
+      :lexers,
+      :lexers!,
+      :filters,
+      :styles,
+      :css,
+      :lexer_name_for,
+      :highlight,
+      # not public but documented
+      :start,
+      # only for testing
+      :size_check,
+      :get_fixed_bits_from_header,
+      :add_ids
+  end
 end
